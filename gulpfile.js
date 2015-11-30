bac411a968c9252be92d6e808c1b25db8ab762fe
Ates Goral <ates.g@myplanet.com>
2015-11-30 09:02:11 -0500

    Read page date from page.yml

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,24 @@
-var gulp = require('gulp'),
+var fs = require('fs'),
+    yaml = require('js-yaml'),
+    gulp = require('gulp'),
     plugins = require('gulp-load-plugins')();
 
 gulp.task('deploy-html', function () {
+    var page = yaml.safeLoad(fs.readFileSync('gh-pages/page.yml', 'utf8'));
+
+    var replacements = {
+        title: page.title
+    };
+
+    if (page.subtitle) {
+        replacements.subtitle = {
+            src: page.subtitle,
+            tpl: '<h2>%s</h2>'
+        };
+    }
+
     gulp.src('gh-pages/index.html')
-        .pipe(plugins.htmlReplace())
+        .pipe(plugins.htmlReplace(replacements))
         .pipe(gulp.dest('.deploy'));
 });
 
