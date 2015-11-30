2d96e4a3f40165356d46874b72744039428be41a
Ates Goral <ates.g@myplanet.com>
2015-11-30 00:56:47 -0500

    Added gulp-html-replace

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,10 @@
-var gulp = require('gulp');
-
+var gulp = require('gulp'),
+    plugins = require('gulp-load-plugins')();
 
 gulp.task('deploy-html', function () {
-
+    gulp.src('gh-pages/index.html')
+        .pipe(plugins.htmlReplace())
+        .pipe(gulp.dest('.deploy'));
 });
 
 gulp.task('deploy', [ 'deploy-html' ]);
